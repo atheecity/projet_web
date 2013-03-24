0454275b730fd896a10f71583b4d9edf95606bf0
charles <cmolin21@gmail.com>
2013-03-24 23:55:48 +0100

    Création class Textarea

---
+++ noyau/formulaireModule/function/Textarea.php
@@ -0,0 +1,54 @@
+<?php
+
+class Textarea
+{
+    
+    private $textarea = '';
+    //Propriete valide pour un textarea
+    private $propriete = array(
+                               'class' => '',
+                               'cols' => '',
+                               'disabled' => '',
+                               'id' => '',
+                               'name' => '',
+                               'readonly' => '',
+                               'rows' => '',
+                               'style' => '');
+    
+    function __construct()
+    {
+        $this->textarea .= '<textarea ';
+    }
+    
+    function addPropriete($prop)
+    {
+        try 
+        {
+            //Test si la propriété est valide pour un textarea
+            if(count(array_diff_key($prop, $this->propriete)) > 0)
+                throw new Erreur("La propriété n'existe pas", 1);
+            
+            //Parcour des différents propriété
+            foreach($prop as $cle=>$valeur)
+            {
+                $this->textarea .= $cle.'="'.$valeur.'" ';
+            }
+        } 
+        catch(Erreur $e) 
+        {
+            return false;
+        }
+        $this->__destruct();
+        return true;
+    }
+    
+    function __toString()
+    {
+        return $this->textarea;
+    }
+    
+    function __destruct()
+    {
+        $this->textarea .= '></textarea>';
+    }
+}
