2a8d9e9893fe75f1b007bc3cbd7d2a9db9eea4c2
Charles Molin <cmolin21@gmail.com>
2013-05-13 08:08:53 +0200

    Modification

--- module/defaultModule/controller/DefaultController.php
+++ module/defaultModule/controller/DefaultController.php
@@ -5,7 +5,12 @@ class DefaultController extends Controller
 	
 	function home()
 	{
-		$this->renderView('module/defaultModule/views/home.html.twig');
+		$ini = new Ini('../config/parameters.ini');
+        $var = $ini->return_array();
+        if(array_key_exists('DATABASE', $var) && array_key_exists('SITE', $var))
+			$this->renderView('module/defaultModule/views/home.html.twig');
+		else
+			header('Location: '.BASE_URL.DS.'configuration_webPlane1.0'.DS.'home'.DS);
 	}
 
 }
